1c5910a7f765ad9f3a1fe761c19c702f4d39cc30
Henrique Calixto <prghenriquetec@gmail.com>
2024-03-25 09:10:16 -0300

    first commit

---
+++ ex002/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exemplo PHP</title>
+</head>
+<body>
+    <?php 
+        $nome = "Henrique";
+        $sobrenome = "Calixto";
+        const PAIS = "Brasil";
+        
+
+        echo "Muito Prazer, $nome $sobrenome! Você mora no " . PAIS ;
+    ?> 
+</body>
+</html>
